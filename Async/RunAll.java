b2f115fdc5666b774ca5ce5cb229de25f5b10084
Daniel <dptorri@gmail.com>
2022-11-17 22:26:19 +0000

    Runnable Sync and Async tasks added

---
+++ Async/RunAll.java
@@ -0,0 +1,13 @@
+package Async;
+
+import java.util.concurrent.ExecutionException;
+
+
+public class RunAll {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        A_RunSynchronousTasks.run();
+        B_RunExecutorTasks.run();
+        C_RunAsyncTasks.run();
+    }
+}
