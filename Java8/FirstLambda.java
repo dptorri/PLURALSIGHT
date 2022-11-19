741285582aa1bd8f585e0d8db4ea9a0a12a61de8
Daniel <dptorri@gmail.com>
2022-11-19 21:35:16 +0000

    Add runnableLambda

--- Java8/FirstLambda.java
+++ Java8/FirstLambda.java
@@ -7,18 +7,22 @@ import java.io.FileFilter;
 public class FirstLambda {
     public static void main( String[] args) {
 
-        FileFilter filter = new FileFilter() {
-            /*The type new FileFilter(){} must implement 
-            the inherited abstract method FileFilter.accept(File) */
+        // FileFilter filter = new FileFilter() {
+        //     /*The type new FileFilter(){} must implement 
+        //     the inherited abstract method FileFilter.accept(File) */
 
-            @Override
-            public boolean accept(File pathname){
-                return pathname.getName().endsWith(".md");
-            }
-        };
+        //     @Override
+        //     public boolean accept(File pathname){
+        //         return pathname.getName().endsWith(".md");
+        //     }
+        // };
 
+        FileFilter filter = (File file) -> file.getName().endsWith(".java");
+        //Result: Java8.FirstLambda 
         File dir = new File("/workspaces/PLURALSIGHT");
         File[] files = dir.listFiles(filter);
+
+
 
         for (File f: files){
             System.out.println(f);
---
+++ Java8/RunnableLambda.java
@@ -0,0 +1,45 @@
+package Java8;
+
+public class RunnableLambda {
+    public static void main(String... args) throws InterruptedException {
+       /*
+        * The Runnable interface should be implemented by any 
+        class whose instances are intended to be executed by a thread. 
+        The class must define a method of no arguments called run.
+        */
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run(){
+                for (int i=0; i< 3; i++){
+                    System.out.println(
+                        "Hello from thread [" +
+                        Thread.currentThread().getName() +
+                        "]"                
+                    );
+                };
+
+            }            
+        };
+
+        Runnable runnableLambda = () -> {
+            for (int i=0; i< 3; i++){
+                System.out.println(
+                    "Hello from thread [" +
+                    Thread.currentThread().getName() +
+                    "]"                
+                );
+            };
+        };
+
+
+        Thread t = new Thread(runnableLambda);
+        t.start();
+        t.join();
+/*
+Hello from thread [Thread-0]
+Hello from thread [Thread-0]
+Hello from thread [Thread-0]
+ */
+
+    }
+}
